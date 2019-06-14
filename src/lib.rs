49964ef2893ac64fe63d5708a04f72f5ef940eac
Chris Pearce <christopher.james.pearce@gmail.com>
2019-06-14 13:24:08 +0200

    Extracted scene loader and added new scene

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-use nalgebra::{Vector3, Point3};
-
-use crate::sphere::Sphere;
-use crate::material::Material;
-use crate::scene::Scene;
-use crate::camera::Camera;
 use crate::tracer::Tracer;
 use crate::canvas_renderer::CanvasRenderer;
 use wasm_bindgen::prelude::*;
@@ -15,91 +9,11 @@ mod camera;
 mod scene;
 mod tracer;
 mod canvas_renderer;
+mod scene_loader;
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    let bright_light = Material::new(
-        Vector3::new(0.0, 0.0, 0.0),
-        1.0,
-        1.0,
-        Vector3::new(3000.0, 3000.0, 3000.0),
-        Vector3::new(0.0, 0.0, 0.0),
-        0.0,
-        0.0
-    );
-
-    let white_lambert = Material::new(
-        Vector3::new(1.0, 1.0, 1.0),
-        1.0,
-        0.0,
-        Vector3::new(0.0, 0.0, 0.0),
-        Vector3::new(0.03, 0.03, 0.03),
-        0.0,
-        0.0
-    );
-
-    let blue_plastic = Material::new(
-        Vector3::new(0.1, 0.1, 1.0),
-        1.0,
-        0.0,
-        Vector3::new(0.0, 0.0, 0.0),
-        Vector3::new(0.04, 0.04, 0.04),
-        0.0,
-        0.2
-    );
-
-    let red_plastic = Material::new(
-        Vector3::new(1.0, 0.0, 0.0),
-        1.0,
-        0.0,
-        Vector3::new(0.0, 0.0, 0.0),
-        Vector3::new(0.04, 0.04, 0.04),
-        0.0,
-        0.2
-    );
-
-    let silver = Material::new(
-        Vector3::new(0.972, 0.960, 0.915),
-        1.0,
-        0.0,
-        Vector3::new(0.0, 0.0, 0.0),
-        Vector3::new(0.972, 0.960, 0.915),
-        0.9,
-        1.0
-    );
-
-    let glass = Material::new(
-        Vector3::new(0.0, 0.0, 0.0),
-        1.6,
-        1.0,
-        Vector3::new(0.0, 0.0, 0.0),
-        Vector3::new(0.04, 0.04, 0.04),
-        0.0,
-        0.0
-    );
-
-    let objects = vec![
-        Sphere::new(Point3::new(-1005.0, 0.0, -8.0), 1000.0, blue_plastic),
-        Sphere::new(Point3::new(1005.0, 0.0, -8.0), 1000.0, red_plastic),
-        Sphere::new(Point3::new(0.0, -1003.0, -8.0), 1000.0, white_lambert),
-        Sphere::new(Point3::new(0.0, 1003.0, -8.0), 1000.0, white_lambert),
-        Sphere::new(Point3::new(0.0, 0.0, -1010.0), 1000.0, white_lambert),
-        Sphere::new(Point3::new(0.0, 13.0, -8.0), 10.5, bright_light),
-        Sphere::new(Point3::new(1.0, -2.0, -7.0), 1.0, silver),
-        Sphere::new(Point3::new(-0.75, -2.0, -5.0), 1.0, glass)
-    ];
-
-    let camera = Camera::new(
-        Point3::new(0.0, 0.0, 7.0),
-        0.024,
-        0.040,
-        15.0,
-        1.4,
-        0.0,
-        0.0
-    );
-
-    let scene = Scene::new(objects, camera);
+    let scene = scene_loader::load_scene("spheres").unwrap();
     let canvas_renderer = CanvasRenderer::new("canvas");
 
     let tracer = Tracer::new(
